c28f78e85d68183afe98c69392531ee395f9e831
Plusb Preco <plusb21@gmail.com>
2017-05-24 11:41:09 +0900

    Add more tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,35 @@
 import pcancel from '.';
 
 describe('`pcancel()`', () => {
-  test('should return `function`', () => {
+  test('should return wrpped function', () => {
     const fn = pcancel(delay);
     expect(fn).toBeInstanceOf(Function);
+  });
+});
+
+describe('`pcancel()` wrapper', () => {
+  test('should return object that contains `Promise` instance and `cancel()` function', () => {
+    const delayC = pcancel(delay);
+    const p = delayC(4000);
+
+    expect(p.promise).toBeInstanceOf(Promise);
+    expect(p.cancel).toBeInstanceOf(Function);
+  });
+
+  test('should handle original promise', async () => {
+    const delayC = pcancel(delay);
+    const p = delayC(4000);
+
+    await expect(p.promise).resolves.toBe('foo');
+  });
+
+  test('should handle promise cancellation', async () => {
+    const delayC = pcancel(delay);
+    const p = delayC(4000);
+
+    p.cancel();
+
+    await expect(p.promise).rejects.toEqual(new Error('Promise has canceled'));
   });
 });
 
